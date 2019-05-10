20eca1c65c5608fc0f3c632c4dfef21e4619e396
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-10 15:54:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,15 +5,15 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        first_friend = 
         {
           :name => "Benvolio",
-          :age => "17"
+          :age => "17",
           :attitude => "worried"
         },
-        second_friend = 
         {
-          
+          :name => "Mercutio",
+          :age => "18"
+          :attitude => "hot-headed"
         }
         #your hashes here!
       ]
@@ -23,6 +23,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44"
+          :attitude => "worried"
+        }
         #your hashes here!
       ]
    }
